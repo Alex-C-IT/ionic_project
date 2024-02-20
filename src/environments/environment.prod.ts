2634270023b04b9bceba7b6f1f9768a29536f8a5
Alex-C-IT <chanot.alexandre@gmail.com>
2024-02-20 16:17:04 +0100

    Modifications pour pouvoir build vers android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDYWCAUP8Dn7gb6eCdAQIokKMgsO8cyMTY",
+    authDomain: "ionic-project-157e3.firebaseapp.com",
+    projectId: "ionic-project-157e3",
+    storageBucket: "ionic-project-157e3.appspot.com",
+    messagingSenderId: "774293040608",
+    appId: "1:774293040608:web:f100a0e898f35d1d874f85"  
+  },
 };
